5601d8b6a2760f0aeafda34522c689439b53cfcf
Sourcery AI <>
2023-01-26 15:55:54 +0000

    'Refactored by Sourcery'

--- docs/demos/multi-page-nested-folders/app.py
+++ docs/demos/multi-page-nested-folders/app.py
@@ -69,9 +69,7 @@ app.layout = dbc.Container(
     State("offcanvas", "is_open"),
 )
 def toggle_theme_offcanvas(n1, is_open):
-    if n1:
-        return not is_open
-    return is_open
+    return not is_open if n1 else is_open
 
 
 if __name__ == "__main__":
--- docs/demos/multi-page-nested-folders/pages/heatmaps.py
+++ docs/demos/multi-page-nested-folders/pages/heatmaps.py
@@ -23,5 +23,4 @@ layout = html.Div(
 
 @callback(Output("heatmaps-graph", "figure"), Input("heatmaps-medals", "value"))
 def filter_heatmap(cols):
-    fig = px.imshow(df[cols])
-    return fig
+    return px.imshow(df[cols])
